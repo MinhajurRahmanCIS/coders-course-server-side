e70eaf9fdff63759aac967b663a39a6339f300f5
RkzNew <rahamannovel@gmail.com>
2022-10-25 21:40:10 +0600

    Data load on coders-server

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json')
+
 app.get('/',(req, res) => {
     res.send("Coders API running!!")
 })
 
+
+app.get('/coders-courses',(req, res) => {
+    res.send(courses)
+})
 
 app.listen(port, () => {
     console.log("Coders server running on port",port)
