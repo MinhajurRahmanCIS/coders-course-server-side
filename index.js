b7d4f89f1ba56801f4f2d1fdebe946a9d1507dfc
RkzNew <rahamannovel@gmail.com>
2022-10-25 21:45:06 +0600

    setup the cors to use the API in client site

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json')
 
+app.use(cors());
 app.get('/',(req, res) => {
     res.send("Coders API running!!")
 })
